37a36761a6b06bde1d027395d01462416df9217c
Erika Cowan <erika.cowan@ligo.org>
2016-06-21 10:50:20 -0700

    added in hveto trigger handling loop

--- autovet.py
+++ autovet.py
@@ -19,7 +19,7 @@ from gwpy.segments import DataQualityFlag
 import ConfigParser
 import os,sys
 import subprocess
-
+from os import path
 
 #command line parsing
 parser = argparse.ArgumentParser(description='Argument Parsing')
@@ -32,6 +32,7 @@ parser.add_argument('type_dq_flag', type=str, help='Please enter either hveto, U
 args = parser.parse_args()
 
 #grabbing start/end years, months, and days and storing them in variables
+#this is used in grabbing the directory structure for hveto files
 start_year = int(args.start_date[:4])
 end_year = int(args.end_date[:4])
 start_month = int(args.start_date[4:6])
@@ -45,21 +46,58 @@ if args.gps_start_time < 971574400: #roughly the end of S6
 if args.gps_end_time < args.gps_start_time:
     parser.error("end_time is before gps_start_time")
 
-#Path for location of the hveto triggers. Eventually expand to hveto_directory_path, ovl_dir_path, upv_dir_path    
-print args.directory_path
+###################################################
+#each option (hveto, UPVh, OVL) in the elif loop will grab the triggers and segments for the associated vetoing program, over any given time period. It will then compile all of the segments into one .txt file, and all of the triggers, into one .txt file. These text files will be used to create the single DQ-flag in .xml format
 ###################################################
 
+#choosing to read in hveto!
 if args.type_dq_flag == 'hveto':
-	print 'Data Quality Flag chosen is hveto, stored in the path ' + args.directory_path
+	print 'Data Quality Flag chosen is hveto, stored in the path ' + args.directory_path + '. (Advance to GO and collect $200.)'
+	
+	#open the total_hveto_trigs.txt file, and prepare to write to it!
+	f = open("total_hveto_trigs.txt", "w")
+	
+	#create pattern paths for the trigger segment files to loop over
+	#NOTE TO SELF: create option to specify which trigger files to loop over. default it to '*VETO_SEGS_ROUND*.txt', and then in the --help, specify how to put in your own list of trigger files.
+	pattern_trigs_hveto = os.path.join(args.directory_path, '{}{:02}','{}{:02}{:02}', '*86400-DARM','*VETO_SEGS_ROUND*.txt')	
+	pattern_segs_hveto = os.path.join(args.directory_path, '{}{:02}','{}{:02}{:02}', '*86400-DARM', 'segs.txt')
+
+	#begin for loop that loops over the range of all days/months/years: necessary because of the hveto data file structure
+	for day in range(start_day, end_day + 1):
+		for month in range(start_month, end_month +1)
+			for year in range(start_year, end_year +1):
+				
+				wildcard_trigs_hveto = pattern_trigs_hveto.format(year, month, year, month, day)
+				
+				#grabbing the trigger files
+				for filename in glob.glob(wildcard_trigs_hveto):
+					
+					#loading the triggers in
+					data = numpy.loadtxt(filename)
+
+					#creating and filling arrays to store the data
+					#NOTE TO SELF: if there are <2 lines of data, will get an error. Still need to test.
+					start_time = [data[i,0] for i in range(len(data))]
+					end_time = [data[i,1] for i in range(len(data))]
+
+					#writing the two arrays to total_hveto_trigs.txt
+					for index in range(len(start_time)):
+						f.write(str(start_time[index]) + " " + str(end_time[index]) + "\n")
 
 
 
+
+#choosing to read in UPVh!
 elif args.type_dq_flag == 'UPVh':
-	print 'Data Quality Flag chosen is ' + args.type_dq_flag +', stored in the path ' + args.directory_path
+	print 'Data Quality Flag chosen is ' + args.type_dq_flag +', stored in the path ' + args.directory_path + '. (Advance to GO and collect $200.)'
 
 
+
+
+
+#whoops! you forgot to choose hveto, UPVh, or OVL! 
 else:
-	print 'Did not give correct dq flag. Please choose from hveto, UPVh, OVL in command line'
+	print 'Did not give correct dq flag. Please choose from hveto, UPVh, OVL in command line. (Go to jail. Go directly to Jail. Do not pass Go. DO NOT COLLECT $200.'
 	exit()
 
 
