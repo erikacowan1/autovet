fa5e85cc7e6f2d7eb983b5bf7eb6b7274321cf41
Erika Cowan <erika.cowan@ligo.org>
2016-06-21 10:29:13 -0700

    verified working elif loop for choosing correct DQ flag. Need to add --help option. in the help option, I plan to put the correct working directories for O1 for hveto and UPVh, just in case people forget when trying to run it- maybe I should have the directory options default to something? we'll see.

--- autovet.py
+++ autovet.py
@@ -28,6 +28,7 @@ parser.add_argument('gps_end_time',type=int,help='Please enter GPS end time')
 parser.add_argument('directory_path',type=str,help='Please enter directory path for triggers and segments')
 parser.add_argument('-s','--start_date', type=str, help='Please enter start date in YYYYMMDD format') 
 parser.add_argument('-e','--end_date', type=str, help='Please enter end date in YYYYMMDD format')
+parser.add_argument('type_dq_flag', type=str, help='Please enter either hveto, UPVh, OVL')
 args = parser.parse_args()
 
 #grabbing start/end years, months, and days and storing them in variables
@@ -38,24 +39,6 @@ end_month = int(args.end_date[4:6])
 start_day = int(args.start_date[6:8])
 end_day = int(args.start_date[6:8])
 
-'''
-#command line parsing
-parser = argparse.ArgumentParser(description='Argument Parsing')
-parser.add_argument('start_time',type=int,help='Please enter start_time in UTC')
-parser.add_argument('end_time',type=int,help='Please enter end_time in UTC')
-parser.add_argument('directory_path',type=str,help='Please enter directory path for stored txt/xml files')
-parser.add
-
-parser.add_argument('date',type=str, help='Please enter correct date for xml path number')
-parser.add_argument('start_day', type=int, help='Please enter start day in format DD')
-parser.add_argument('end_day', type=int, help='Please enter end day in format DD')
-parser.add_argument('start_month', type=int, help='Please enter start month in format MM')
-parser.add_argument('end_month', type=int, help='Please enter end month in format MM')
-parser.add_argument('start_year', type=int, help='Please enter year in format YYYY')
-parser.add_argument('end_year', type=int, help='Please enter year in format YYYY')
-#parser.add_argument('type_dq_flag', type=int, help='Please enter either hveto, UPV, OVL')
-'''
-
 #A check to make sure we're within the time window of aLIGO, and that end_time is after start_time
 if args.gps_start_time < 971574400: #roughly the end of S6
     parser.error("gps_start_time before S6")
@@ -64,86 +47,38 @@ if args.gps_end_time < args.gps_start_time:
 
 #Path for location of the hveto triggers. Eventually expand to hveto_directory_path, ovl_dir_path, upv_dir_path    
 print args.directory_path
+###################################################
 
-pattern_trigs_hveto = os.path.join(args.directory_path, '{}{:02}','{}{:02}{:02}', '*86400-DARM','*VETO_SEGS_ROUND*.txt')
-pattern_segs_hveto = pattern = os.path.join(args.directory_path, '{}{:02}','{}{:02}{:02}', '*86400-DARM', 'segs.txt')
+if args.type_dq_flag == 'hveto':
+	print 'Data Quality Flag chosen is hveto, stored in the path ' + args.directory_path
+
+
+
+elif args.type_dq_flag == 'UPVh':
+	print 'Data Quality Flag chosen is ' + args.type_dq_flag +', stored in the path ' + args.directory_path
+
+
+else:
+	print 'Did not give correct dq flag. Please choose from hveto, UPVh, OVL in command line'
+	exit()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###################################################
 '''
-pattern_trigs_UPVh = os.path.join(args.directory_path, 'DARM_LOCK_{}_{}-H', 'H1:*veto.txt')
-pattern_segs_UPVh = os.path.join(args.directory_path, 'DARM_LOCK_{}_{}-H', 'segments.txt')
-
-
-for day in range(args.gps_start_time, args.gps_end_time +1):
-	wildcard_UPVh = pattern_trigs_hveto.format(start_time, end_time)
-	for filename in glob.glob(wildcard_UPV):
-		print filename
-
-		data = numpy.loadtext(filename)
-	print data
-	#come up with better in-for loop dummy variables
-	start_time = [data[i,0] for i in range(len(data))]
-	end_time = [data[i,1] for i in range(len(data))]
-
-	f = open("total_UPVh_segs.txt", "a")
-	for index in range(len(start_time)):
-		f.write(str(start_time[index]) + " " + str(end_time[index]) + "\n")
-
-for day in range(args.gps_start_time, args.gps_end_time +1):
-	wildcard_UPVh_segs = pattern_segs_hveto.format(year, month,year, month, day)
-        for filename in glob.glob(wildcard_UPVh_segs):
-        	knownsegments =numpy.atleast_2d(numpy.loadtxt(filename))
-                known_start = [knownsegments[i,0] for i in range(len(knownsegments))]
-                known_end = [knownsegments[i,1] for i in range(len(knownsegments))]
-
-                f = open("total_UPVh_segs.txt","a")
-                for index in range(len(known_start)):
-                f.write(str(known_start[index]) + " " + str(known_end[index]) + "\n")
-'''
-for day in range(args.start_day, args.end_day + 1):
-    for month in range(args.start_month, args.end_month +1):
-	for year in range(args.start_year, args.end_year +1):
-        	wildcard_hveto = pattern_trigs_hveto.format(year, month, year, month, day)
-        	for filename in glob.glob(wildcard_hveto):
-            		print filename
-            
-            	#for file in filename:
-            	data = numpy.loadtxt(filename)
-           	print data    
-            
-            	#If there is less than 2 lines of data, will get error
-            	start_time = [data[i,0] for i in range(len(data))]
-            	end_time = [data[i,1] for i in range(len(data))]
-                
-            	f = open("total_hveto_segs.txt", "a")
-            	for index in range(len(start_time)):
-                	f.write(str(start_time[index]) + " " + str(end_time[index]) + "\n")
-
-
-
-for day in range(args.start_day, args.end_day + 1):
-    for month in range(args.start_month, args.end_month +1):	
-	for year in range(args.start_year, args.end_year +1):
-	# define known start and end times
-		wildcard_segs = pattern_segs_hveto.format(year, month,year, month, day)
-		for filename in glob.glob(wildcard_segs):
-			knownsegments =numpy.atleast_2d(numpy.loadtxt(filename, delimiter =','))
-			known_start = [knownsegments[i,0] for i in range(len(knownsegments))]
-			known_end = [knownsegments[i,1] for i in range(len(knownsegments))]
-		
-			f = open("total_hveto_segs.txt","a")
-			for index in range(len(known_start)):
-				f.write(str(known_start[index]) + " " + str(known_end[index]) + "\n")
-
-
-# define known start and end times
-try: knownsegments =numpy.atleast_2d(numpy.loadtxt('total_hveto_segs.txt'))
-except:
-        print 'No total_segs.txt file in current working directory. It should have been produced from last loop.' 
-known_start = [knownsegments[i,0] for i in range(len(knownsegments))]
-known_end = [knownsegments[i,1] for i in range(len(knownsegments))]
-
-
-#os.system("mkdir %s_xml" % args.date)
-
 #construct flag and filename
 
 flag_name = 'H1:HVT-'+ args.date +':1' #NEEDS TO BE CHANGED
@@ -190,18 +125,4 @@ config.set('tab-SNR-5.5', 'segmentfile', 'segments_HVT_RND.xml')
 
 with open('hveto_segs.ini','wb') as configfile:
 	config.write(configfile)
-
-#code that can loop over channels while generating the .ini file, not necessary for now
-'''p = 1
-for i in winning_channels:
-    config.add_section("tab-" + i)
-    config.set("tab-" + i, 'name', "Round " + str(p)+ ":" + i)
-    config.set("tab-" + i, 'shortname','Round ' + str(p))
-    config.set("tab-" + i, 'flags','H1:HVT-ER7_RND' + str(p) + ':1')
-    config.set("tab-" + i, 'states','Science')
-    config.set("tab-" + i, 'segmentfile','segments_HVT_RND' + str(p) + '.xml' )
-    p += 1
-    
-    with open('hveto.ini','wb') as configfile:
-        config.write(configfile)     
 '''
