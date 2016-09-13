101441f21e2ec56b300114d6746c01cb6b1e548f
erikacowan1 <erikacowan1@gmail.com>
2016-09-13 01:00:15 -0400

    completely changed online hveto structure

--- autovet.py
+++ autovet.py
@@ -15,8 +15,9 @@ import argparse
 import glob
 import numpy
 from gwpy.segments import DataQualityFlag
-import ConfigParser
+#import ConfigParser
 from gwpy.time import tconvert
+import datetime
 
 #command line parsing
 parser = argparse.ArgumentParser(
@@ -113,13 +114,7 @@ if args.type_dq_flag == 'hveto':
 		f.close()
 
 	elif args.online_offline == 'online':
-		#grabbing start/end years, months, and days and storing them in variables
-		start_year = int(args.start_date[:4])
-		end_year = int(args.end_date[:4])
-		start_month = int(args.start_date[4:6])
-		end_month = int(args.end_date[4:6])
-		start_day = int(args.start_date[6:8])
-		end_day = int(args.end_date[6:8])	
+
 
 		#TRIGGER HANDLING: begin for loop that loops over the range of all days/months/years
 		f = open("total_hveto_trigs.txt", "w") #file that will hold collection of all triggers
@@ -128,27 +123,30 @@ if args.type_dq_flag == 'hveto':
 		#NOTE TO SELF: create option to specify which trigger files to loop over. default it to '*VETO_SEGS_ROUND*.txt', and then in the --help, specify how to put in your own list of trigger files.
 		pattern_trigs_hveto = os.path.join(args.directory_path, '{}{:02}','{}{:02}{:02}', '*86400-DARM','*VETO_SEGS_ROUND*.txt')	
 
-		for day in range(start_day, end_day + 1):
-			for month in range(start_month, end_month +1):
-				for year in range(start_year, end_year +1):
-					
-					wildcard_trigs_hveto = pattern_trigs_hveto.format(year, month, year, month, day)
-					
-					#grabbing the trigger files
-					for filename in glob.glob(wildcard_trigs_hveto):
-					
-						#loading the triggers in
-						data = numpy.atleast_2d(numpy.loadtxt(filename))
+		start_time_utc = tconvert(args.gps_start_time)
+		end_time_utc = tconvert(args.gps_end_time)
 
-						#creating and filling arrays to store the data
-						#NOTE TO SELF: if there are <2 lines of data, will get an error. Still need to test.
-						start_time = [data[i,0] for i in range(len(data))]
-						end_time = [data[i,1] for i in range(len(data))]
-
-						#writing the two arrays to total_hveto_trigs.txt
-						for index in range(len(start_time)):
-							f.write(str(start_time[index]) + " " + str(end_time[index]) + "\n")
-
+		while start_time_utc < end_time_utc:
+    			day = start_time_utc.day
+    			month = start_time_utc.month
+    			year = start_time_utc.year
+    
+    			wildcard_trigs_hveto = pattern_trigs_hveto.format(year, month, year, month, day)
+    
+    			#grabbing the trigger files
+    			for filename in glob.glob(wildcard_trigs_hveto):
+	    			#loading the triggers in
+				data = SegmentList.read(filename)
+        			triggers += data
+        			start_time_utc += datetime.timedelta(days=1)
+        
+    			start_time_utc += datetime.timedelta(days=1)
+    
+		triggers.coalesce()
+		start_end_seg = segment(args.gps_start_time, args.gps_end_time)    
+		triggers = triggers & SegmentList([start_end_seg])    
+        
+		triggers.write("total_hveto_trigs.txt")
 
 
 
